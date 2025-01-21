916088310bc5b23e76ac7bb6c9da43817b3a27f7
CSK <73425927+cr2007@users.noreply.github.com>
2025-01-21 12:17:43 +0400

    Refactored from a form to normal components

--- components/heatMapForm.tsx
+++ components/heatMapForm.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Check, ChevronsUpDown } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { ArrowRightIcon, Check, ChevronsUpDown } from "lucide-react";
+import React from "react";
+import { useState } from "react";
 
 import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import {
   Command,
   CommandEmpty,
@@ -14,246 +21,73 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-
-const formSchema = z.object({
-  timeZone: z.string().nonempty("Please select a timezone."),
-  year: z
-    .string()
-    .min(4, "The year must be a 4 digit value")
-    .max(4, "Year cannot be more than 4 digits")
-    .regex(/^\d{4}$/, "Year must be a valid 4-digit number."),
-  jsonFile: z
-    .instanceof(File)
-    .refine(
-      (file) => file.type === "application/json",
-      "File must be a JSON file."
-    ),
-});
+} from "@/components/ui/command";
 
 export function HeatMapForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Defaults to the user's local time zone
-      year: new Date().getFullYear().toString(), // Defaults to current year
-    },
-  });
+  const [open, setOpen] = React.useState(false);
+  const [timeZone, setTimeZone] = useState(Intl.DateTimeFormat().resolvedOptions().timeZone);
 
-  const { setValue } = form;
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log("Submitted values:", values);
-
-    if (values.jsonFile) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        console.log(`JSON file contents: ${event.target?.result}`);
-      };
-
-      reader.readAsText(values.jsonFile);
-    }
-  }
-
-  const timeZones =
-  typeof Intl.supportedValuesOf === "function"
-  ? Intl.supportedValuesOf("timeZone")
-  : [];
+  const timeZones = Intl.supportedValuesOf("timeZone");
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        {/* File Input Field */}
-        <FormField
-          control={form.control}
-          name="jsonFile"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>JSON File</FormLabel>
-              <FormControl>
-                <Input
-                  id="jsonFile"
-                  type="file"
-                  accept="application/json"
-                  placeholder="Enter the file contents"
-                  onChange={(event) => {
-                    if (event.target.files?.[0]) {
-                      setValue("jsonFile", event.target.files[0], { shouldValidate: true });
-                    }
-                  }}
-                />
-              </FormControl>
-              <FormMessage />
-              <FormDescription>
-                Upload the <code><b>conversation.json</b></code> file in the data export.
-              </FormDescription>
-            </FormItem>
-          )}
-        />
+    <>
+      {/* File Input Field */}
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="jsonFile">JSON Input File (<code>conversations.json</code>)</Label>
+        <Input id="jsonFile" type="file"></Input>
 
-        {/* Time Zone Popover Field */}
-        <FormField
-          control={form.control}
-          name="timeZone"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Time Zone</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
+        <div className="flex flex-col items-center gap-2 text-center bg-secondary text-slate-950 dark:text-slate-300 p-4 rounded-lg">
+					<p className="text-center text-sm">
+						Export your chat history from ChatGPT and upload it here.
+					</p>
+					<p className="text-center text-sm flex items-center gap-1">
+						Profile <ArrowRightIcon className="inline-block w-3 h-3" /> Data Controls <ArrowRightIcon className="inline-block w-3 h-3" /> Export data
+					</p>
+				</div>
+      </div>
+
+      {/* Time Zone Popover Field */}
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {timeZone || "Select timezone"}
+            <ChevronsUpDown className="opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search timezone..." className="h-9" />
+            <CommandList>
+              <CommandEmpty>No timezone found.</CommandEmpty>
+              <CommandGroup>
+                {timeZones.map((tz) => (
+                  <CommandItem
+                    key={tz}
+                    value={tz}
+                    onSelect={() => {
+                      setTimeZone(tz);
+                      setOpen(false);
+                    }}
+                  >
+                    {tz}
+                    <Check
                       className={cn(
-                        "w-[200px] justify-between",
-                        !field.value && "text-muted-foreground"
+                        "ml-auto",
+                        timeZone === tz ? "opacity-100" : "opacity-0"
                       )}
-                    >
-                      {field.value
-                        ? timeZones.find(
-                            (tz) => tz === field.value
-                          )
-                        : "Select language"}
-                      <ChevronsUpDown className="opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput
-                      placeholder="Search framework..."
-                      className="h-9"
                     />
-                    <CommandList>
-                      <CommandEmpty>No framework found.</CommandEmpty>
-                      <CommandGroup>
-                        {timeZones.map((tz) => (
-                          <CommandItem
-                            value={tz}
-                            key={tz}
-                            onSelect={() => {
-                              form.setValue("timeZone", tz)
-                            }}
-                          >
-                            {tz}
-                            <Check
-                              className={cn(
-                                "ml-auto",
-                                tz === field.value
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormDescription>
-                The timezone that will be used for assigning the conversations with the heatmap
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-
-        {/* <FormField
-          control={form.control}
-          name="timeZone"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Time Zone</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        "w-[200px] justify-between",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value || "Select language"}
-                      <ChevronsUpDown className="opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] p-0">
-                  <Command>
-                    <CommandInput
-                      placeholder="Search Time Zone..."
-                      className="h-9"
-                      onInput={(event) => {
-                        console.log("Command Input Value:", event.currentTarget.value)
-                      }}
-                    />
-                    <CommandList>
-                      <CommandEmpty>No Time Zone found.</CommandEmpty>
-                      <CommandGroup>
-                        {timeZones.map((timeZone) => (
-                          <CommandItem
-                            value={timeZone}
-                            key={timeZone}
-                            onSelect={() => {
-                              console.log("Selected Time Zone:", timeZone);
-                              form.setValue("timeZone", timeZone, {shouldValidate: true});
-                            }}
-                          >
-                            {timeZone}
-                            <Check
-                              className={cn(
-                                "ml-auto",
-                                timeZone === field.value
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <FormDescription>
-                This will be the time zone for creating the heat map.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        /> */}
-
-        {/* Year Input Field */}
-        <FormField
-          control={form.control}
-          name="year"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Year</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter the year" type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Generate Heatmap</Button>
-      </form>
-    </Form>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
